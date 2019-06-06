6f63dff9d83d6a19e0fcf52308fd20aa032797ce
Hermann Hueck <hermann.hueck@mailbox.org>
2019-06-06 20:04:43 +0200

    added example 'whitebox'

---
+++ libMacros/src/main/scala/gurnell/ex10whitebox/WhiteboxMacros.scala
@@ -0,0 +1,24 @@
+package gurnell.ex10whitebox
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.{Context => BlackboxContext}
+import scala.reflect.macros.whitebox.{Context => WhiteboxContext}
+
+object WhiteboxMacros {
+
+  def blackboxOptionOf[A](value: A): Option[A] =
+    macro blackboxOptionOfMacro[A]
+
+  def whiteboxOptionOf[A](value: A): Option[A] =
+    macro whiteboxOptionOfMacro[A]
+
+  def blackboxOptionOfMacro[A: c.WeakTypeTag](c: BlackboxContext)(value: c.Tree): c.Tree = {
+    import c.universe._
+    q"Some($value)"
+  }
+
+  def whiteboxOptionOfMacro[A: c.WeakTypeTag](c: WhiteboxContext)(value: c.Tree): c.Tree = {
+    import c.universe._
+    q"Some($value)"
+  }
+}
