6bd6d6fbf9c30351c7e9c491e7c9e9173972ca17
Hermann Hueck <hermann.hueck@mailbox.org>
2019-08-20 22:56:27 +0200

    code formatted

--- libMacros/src/main/scala/scalalang/printf/Macros.scala
+++ libMacros/src/main/scala/scalalang/printf/Macros.scala
@@ -12,25 +12,32 @@ object Macros {
   def printf_impl(c: blackbox.Context)(format: c.Expr[String], params: c.Expr[Any]*): c.Expr[Unit] = {
 
     import c.universe._
+
     val Literal(Constant(s_format: String)) = format.tree
 
-    val evals = ListBuffer[ValDef]()
+    val evals: ListBuffer[ValDef] = ListBuffer[ValDef]()
+
     def precompute(value: Tree, tpe: Type): Ident = {
       val freshName = TermName(c.freshName("eval$"))
       evals += ValDef(Modifiers(), freshName, TypeTree(tpe), value)
       Ident(freshName)
     }
 
-    val paramsStack = mutable.Stack[Tree](params map (_.tree): _*)
+    val paramsStack: mutable.Stack[Tree] = mutable.Stack[Tree](params map (_.tree): _*)
 
-    val refs = s_format.split("(?<=%[\\w%])|(?=%[\\w%])") map {
+    val formatStrings: Array[String] = s_format.split("(?<=%[\\w%])|(?=%[\\w%])")
+
+    val refs: Array[c.universe.Tree] = formatStrings map {
       case "%d" => precompute(paramsStack.pop, typeOf[Int])
       case "%s" => precompute(paramsStack.pop, typeOf[String])
       case "%%" => Literal(Constant("%"))
       case part => Literal(Constant(part))
     }
 
-    val stats = evals ++ refs.map { ref => reify(print(c.Expr[Any](ref).splice)).tree }
+    val stats: ListBuffer[Tree] = evals ++ refs.map { ref =>
+      reify(print(c.Expr[Any](ref).splice)).tree
+    }
+
     c.Expr[Unit](Block(stats.toList, Literal(Constant(()))))
   }
 }
